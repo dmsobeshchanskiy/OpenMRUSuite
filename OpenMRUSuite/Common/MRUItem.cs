a87d9a4e295fb9673dfcd7bdb215bad1f869b391
Sobeshchanskiy Dmitriy Mikhaylovich <dsobeshchanskiy@gmail.com>
2019-04-07 16:24:46 +0300

    Add MRUItem class

---
+++ OpenMRUSuite/Common/MRUItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenMRUSuite.Common
+{
+    public class MRUItem
+    {
+        public string FilePath { get; set; }
+        public int SelectedCount { get; set; }
+        public int Pinned { get; set; }
+    }
+}
