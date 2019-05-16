0a7d31af4e37b0d2e5377d25b8fdd68ac4b307e3
Sobeshchanskiy Dmitriy Mikhaylovich <dsobeshchanskiy@gmail.com>
2019-05-16 23:35:05 +0300

    Ver. 0.9.0. Item GUI design adjust

--- MRUGuiWin/MRUItemControl.Designer.cs
+++ MRUGuiWin/MRUItemControl.Designer.cs
@@ -48,9 +48,9 @@
             this.labelFileName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.labelFileName.Location = new System.Drawing.Point(54, 4);
             this.labelFileName.Name = "labelFileName";
-            this.labelFileName.Size = new System.Drawing.Size(226, 23);
+            this.labelFileName.Size = new System.Drawing.Size(226, 28);
             this.labelFileName.TabIndex = 1;
-            this.labelFileName.Text = "File name";
+            this.labelFileName.Text = "File name g";
             this.labelFileName.Click += new System.EventHandler(this.MRUItemControl_Click);
             this.labelFileName.MouseLeave += new System.EventHandler(this.MRUItemControl_MouseLeave);
             // 
